02714b1698e73a4e298fb2ebeaa150663d7d7846
Kyoichiro Yamada <yamada@wacul.co.jp>
2017-10-22 17:31:16 +0900

    fix misspell

--- editor/tty.go
+++ editor/tty.go
@@ -10,7 +10,7 @@ const forceColorFlag = "RICHGO_FORCE_COLOR"
 
 // Formattable judge whether a descriptor (like a os.Stdout, os.Stderr or *os.File...)
 // is capable of colorization or not. The default behavior is to detect whether
-// it is connected to a TTY, but may be overriden by setting the environment
+// it is connected to a TTY, but may be overridden by setting the environment
 // variable `RICHGO_FORCE_COLOR` to a non-empty value.
 func Formattable(descriptor interface {
 	Fd() uintptr
