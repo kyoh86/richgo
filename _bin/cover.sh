8ec8e2478605be83696151f87fecf98f867089dd
Kyoichiro Yamada <yamada@wacul.co.jp>
2017-11-24 18:34:26 +0900

    fix coverage

--- _bin/cover.sh
+++ _bin/cover.sh
@@ -1,16 +1,16 @@
+set -ex
+
 echo "mode: set" > acc.cover
-for import_path in `go list ./... | grep -Fve 'sample'`; do
-  directory=$GOPATH/src/$import_path
-  if ls $directory/*.go &> /dev/null; then
-    go test -coverprofile=profile.cover $import_path
-    if [ -f profile.cover ]; then
-      cat profile.cover | grep -Fve "mode: set" >> acc.cover
-    fi
+
+for import_path in `go list ./...`; do
+  go test -coverprofile=profile.cover $import_path
+  if [ -f profile.cover ]; then
+    cat profile.cover | grep -Fve "mode: set" >> acc.cover
+    rm -f profile.cover
   fi
 done
 
-rm -f profile.cover
 if [ -n "${COVERALLS_TOKEN}" ]; then
   goveralls -coverprofile=acc.cover -repotoken=$COVERALLS_TOKEN -service=wercker.com
-  rm -f acc.cover
 fi  
+rm -f acc.cover || :
